e63b841e8980f4adee24cf475e60d5064495b2dd
joaovprimo <fulano@email.com>
2022-06-23 17:00:57 -0300

    atualizado02 - 23/06 - Conferindo se o usuario está  On  e avisa que o usuario entrou

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 let pessoa = [];
 let login;
+let data = new Date();
+let horas = data.getHours();
+let minutos = data.getMinutes();
+let segundos = data.getSeconds();
+let hhmmss = [horas, minutos, segundos].join(':');
 
 entrarNaPagina()
 function entrarNaPagina (){
@@ -18,7 +23,11 @@ promise.catch(tratarErro);
 
 function tratarSucesso(sucesso){
 let qlsucesso = sucesso.status;
-    console.log( 'sucesin');
+    alert (`seja bem-vindo ${login}`);
+   let entrou = document.querySelector('.caixamensagens');
+   entrou.innerHTML = `<li class="status"> <div class="texto">
+   <div class="hora">${(hhmmss)}</div> <div class="login"> ${login} </div> entrou na sala..</div></li>`;
+   setInterval(confirmaConexao,5000);
 }
 
 function tratarErro(erro){
@@ -31,4 +40,19 @@ function tratarErro(erro){
     }
     enviaNome();
 }
+}
+
+function confirmaConexao(){
+    let promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/status', pessoa);
+    promise.then(continuaON);
+    promise.catch(deslogou);
+}
+
+function continuaON (taonline){
+console.log('continua Online'); 
+}
+
+function deslogou (saiu) {
+let statusoff = saiu.response.status;
+console.log("saiu da sala");
 }
