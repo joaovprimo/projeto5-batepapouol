149374dee996a5fd6bf813ff08846f50d65ef1ab
joaovprimo <fulano@email.com>
2022-06-24 17:41:06 -0300

    atualizado02 - 24/06 - mensagens plotadas no vizualizador

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 let pessoa = [];
 let login;
-let data = new Date();
-let horas = data.getHours();
-let minutos = data.getMinutes();
-let segundos = data.getSeconds();
-let hhmmss = [horas, minutos, segundos].join(':');
-let mensagem;
 
 entrarNaPagina()
 function entrarNaPagina (){
@@ -17,20 +11,19 @@ function entrarNaPagina (){
 }
 
 function enviaNome(){
-let promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', pessoa);
+const promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', pessoa);
 promise.then(tratarSucesso);
 promise.catch(tratarErro);
 }
 
 function tratarSucesso(sucesso){
-let qlsucesso = sucesso.status;
-console.log(`${login}`)
+console.log(`${login}`);
     setInterval(recebeMsg,3000);
-   setInterval(confirmaConexao,5000);
+    setInterval(confirmaConexao,5000);
 }
 
 function tratarErro(erro){
-   let qlerro = erro.response.status;
+   const qlerro = erro.response.status;
    console.log(qlerro);
    if(qlerro === 400){
     login = prompt ("Põe outro ai, esse ja foi usado");
@@ -41,34 +34,50 @@ function tratarErro(erro){
 }
 }
 function recebeMsg(){
- let listaDeMensagem = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+ const listaDeMensagem = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
  listaDeMensagem.then(processaMensagem);
 }
 
 function processaMensagem(mensagem){
-    let mensg = document.querySelector('.caixamensagens').innerHTML;
-    mensg="";
-    if(mensagem.type === 'status'){
-   mensg += `<li class="status"> <div class="texto">
-   <div class="hora">${mensagem.time}</div> <div class="login"> ${mensagem.from} </div> ${mensagem.text}</div></li>`;
+    const mensg = document.querySelector('.caixamensagens');
+    let data = mensagem.data;
+    mensg.innerHTML ="";
+    for(let i=0; i<data.length;i++){
+    if(data[i].type==='status'){
+    mensg.innerHTML+= `<li class="status"> 
+        <div class="texto">
+        <div class="hora">${data[i].time}</div>
+        <div class="login"> ${data[i].from} </div> 
+        ${data[i].text}</div>
+    </li>`;
     }
-    if(mensagem.type ==="message"){
-       mensg += `<li class="normais"><div class="texto">
-       <div class="hora">${mensagem.time}</div>
-       <div class="nome"> ${mensagem.from} </div> 
-       para  <div class="nome"> ${mensagem.to}</div>: ${mensagem.text}</div></li>`;
+    if(data[i].type ==="message"){
+       mensg.innerHTML +=
+    `<li class="normais">
+        <div class="texto">
+        <div class="hora">${data[i].time}</div>
+        <div class="nome"> ${data[i].from} </div> 
+        para  
+        <div class="nome"> ${data[i].to}</div>
+        : ${data[i].text}</div>
+    </li>`;
     }
-    if(mensagem.type ==="private_message"){
-        mensg += `<li class="reservado"><div class="texto">
-        <div class="hora">${mensagem.time}</div>
-        <div class="nome"> ${mensagem.from} </div> 
-        reservadamente para  <div class="nome"> ${mensagem.to}</div>: ${mensagem.text}</div></li>`;
+    if(data[i].type ==="private_message"){
+        mensg.innerHTML += 
+    `<li class="reservado">
+        <div class="texto">
+        <div class="hora">${data[i].time}</div>
+        <div class="nome"> ${data[i].from} </div> 
+        reservadamente para  
+        <div class="nome"> ${data[i].to}</div>:
+        ${data[i].text}</div>
+     </li>`;
      }
     }
-
+}
 
 function confirmaConexao(){
-    let promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/status', pessoa);
+    const promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/status', pessoa);
     promise.then(continuaON);
     promise.catch(deslogou);
 }
